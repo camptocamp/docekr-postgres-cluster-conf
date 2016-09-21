ee13aba8151385bb51139b008b8d033b15d43b8e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-09-21 15:52:08 +0200

    Add debug message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ -s "$PGDATA/PG_VERSION" ]; then
+  echo "PosgreSQL cluster already initialized."
   # internal start of server in order to allow set-up using psql-client
   # does not listen on external TCP/IP and waits until start finishes
   gosu postgres pg_ctl -D "$PGDATA" \
@@ -18,6 +19,8 @@ if [ -s "$PGDATA/PG_VERSION" ]; then
   done
 
   gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+else
+  echo "PostgreSQL cluster not yet initialized."
 fi
 
 cp -f /postgres-c2c-conf/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d
