354dae6e510520edea47c0d462edcc84a23f3fa9
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-09-21 13:25:17 +0200

    Grant execution on pg_terminate_backend and pg_backend_pid to healthcheck

--- docker-entrypoint-initdb.d/15-healthcheck-database.sql
+++ docker-entrypoint-initdb.d/15-healthcheck-database.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE healthcheck;
 CREATE USER healthcheck;
-\c healthcheck
+\c healthcheck -- psql specific command
 DROP SCHEMA public;
 REVOKE ALL ON ALL FUNCTIONS IN SCHEMA pg_catalog FROM public;
 GRANT EXECUTE ON FUNCTION pg_is_in_recovery() to healthcheck;
+GRANT EXECUTE ON FUNCTION pg_terminate_backend(int) to healthcheck;
+GRANT EXECUTE ON FUNCTION pg_backend_pid() to healthcheck;
