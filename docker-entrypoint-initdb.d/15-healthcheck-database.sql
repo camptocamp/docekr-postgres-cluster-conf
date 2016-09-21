7051e9eb01a8723719ae8c63da65f300469c8b0c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-09-21 15:51:53 +0200

    Remove comment

--- docker-entrypoint-initdb.d/15-healthcheck-database.sql
+++ docker-entrypoint-initdb.d/15-healthcheck-database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE healthcheck;
 CREATE USER healthcheck;
-\c healthcheck -- psql specific command
+\c healthcheck
 DROP SCHEMA public;
 REVOKE ALL ON ALL FUNCTIONS IN SCHEMA pg_catalog FROM public;
 GRANT EXECUTE ON FUNCTION pg_is_in_recovery() to healthcheck;
