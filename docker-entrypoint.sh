6dc9dd457d48380b45f4972058fddbd322e6703e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-09-21 11:36:35 +0200

    Allow to run SQL script at runtime (not only at initialization time)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-DIR=/postgres-c2c-conf/docker-entrypoint.d
+if [ -s "$PGDATA/PG_VERSION" ]; then
+  # internal start of server in order to allow set-up using psql-client
+  # does not listen on external TCP/IP and waits until start finishes
+  gosu postgres pg_ctl -D "$PGDATA" \
+    -o "-c listen_addresses='localhost'" \
+    -w start
 
-if test -d "$DIR"
-then
-  /bin/run-parts --verbose "$DIR"
+  for f in /postgres-c2c-conf/docker-entrypoint.d/*; do
+    case "$f" in
+      *.sh)     echo "$0: running $f"; . "$f" ;;
+      *.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
+      *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+      *)        echo "$0: ignoring $f" ;;
+    esac
+    echo
+  done
+
+  gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
 fi
 
 cp -f /postgres-c2c-conf/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d
