de1973273dab027431cce9f123ed2b36e7ea6fa7
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-09-23 17:49:28 +0200

    Update POSTGRES_USER's password

--- docker-entrypoint.d/10-fix-pg_hba.sh
+++ docker-entrypoint.d/10-fix-pg_hba.sh
@@ -8,6 +8,7 @@ else
   authMethod=trust
 fi
 
+# Fix pg_hba.conf
 if grep -q "^host all all 0.0.0.0/0 " "$PGDATA/pg_hba.conf"; then
   if grep -q "^host all all 0.0.0.0/0 $authMethod$" "$PGDATA/pg_hba.conf"; then
     echo "pg_hba.conf already valid"
@@ -18,3 +19,11 @@ else
   { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
 fi
 
+# Fix user password
+if psql -U postgres -c "SELECT COUNT(*) AS count FROM pg_catalog.pg_user WHERE usename = '$POSTGRES_USER'" | grep -q 1; then
+  op="ALTER"
+else
+  op="CREATE"
+fi
+
+psql -Upostgres -c "$op USER \"$POSTGRES_USER\" WITH SUPERUSER $pass ;"
